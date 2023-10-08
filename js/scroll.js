8139af65134d1666a946cae24ba3b1ce407e45b1
Spokay <hugobressange@gmail.com>
2023-10-08 16:21:40 +0200

    ci: progress bar implemented and card fixed

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,33 @@
 let container = document.querySelector("#scroll-container");
+let landingPage = document.querySelector(".landing");
+let pages = document.querySelectorAll(".page");
+let range = document.querySelector(".range");
+let rangeLabel = document.querySelector(".range__label");
+let totalWidth = 0;
+
+pages.forEach((page) => totalWidth += page.offsetWidth.valueOf());
+
+range.style.width = totalWidth+"px";
+window.addEventListener("resize", function (){
+    range.style.width = totalWidth+"px";
+});
+
 container.addEventListener("wheel", function (e) {
     e.preventDefault();
     container.scrollLeft += e.deltaY.valueOf();
+});
+
+container.addEventListener("scroll", function (e) {
+    let percentValue = (container.scrollLeft.valueOf() / (totalWidth - landingPage.offsetWidth.valueOf())) * 100;
+    if (percentValue === 100){
+        e.preventDefault();
+        range.style.setProperty("--p", percentValue.toString());
+        rangeLabel.innerText = percentValue.toString().concat("%");
+    }else if (percentValue < 100){
+        console.log(percentValue);
+        range.style.setProperty("--p", percentValue.toString());
+        rangeLabel.innerText = Math.round(percentValue).toString().concat("%");
+    }
 });
 
 
